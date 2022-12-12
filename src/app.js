3968d6d97f5ce307ad910ba35b464a027d325b8a
SennCode <andres.alonso.senn@gmail.com>
2022-12-12 12:03:31 +0000

    random card

--- src/app.js
+++ src/app.js
@@ -1,11 +1,58 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const icons = ["♥", "♠", "♣", "♦"];
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "K",
+  "Q"
+];
+
+let topSuitEl;
+let numberEl;
+let bottomSuitEl;
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  topSuitEl = document.querySelector(".top-suit");
+  numberEl = document.querySelector(".number");
+  bottomSuitEl = document.querySelector(".bottom-suit");
+  const card = document.querySelector(".card");
+  // card.style.width = "200px";
+  const inputChange = document.querySelector("#input-change");
+  inputChange.addEventListener("keyup", _ => {
+    console.log({ value: inputChange.value });
+    const v = inputChange.value;
+    card.style.width = `${v}px`;
+    document.getElementById("info").innerText = v;
+  });
+
+  const btn = document.getElementById("btn-input");
+  btn.addEventListener("click", crearCarta);
+  crearCarta();
 };
+
+function crearCarta() {
+  const icon = icons[Math.floor(Math.random() * icons.length)];
+  const value = values[Math.floor(Math.random() * values.length)];
+  if (icon === "♥" || icon === "♦") {
+    topSuitEl.style.color = "red";
+    numberEl.style.color = "red";
+    bottomSuitEl.style.color = "red";
+  } else {
+    topSuitEl.style.color = "black";
+    numberEl.style.color = "black";
+    bottomSuitEl.style.color = "black";
+  }
+  topSuitEl.innerHTML = icon;
+  numberEl.innerHTML = value;
+  bottomSuitEl.innerHTML = icon;
+}
